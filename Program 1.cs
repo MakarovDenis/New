0b2cf8b476475e680e1ec242ab2e5f33692af3d6
MakarovDenis <52467436+MakarovDenis@users.noreply.github.com>
2019-07-06 19:45:30 +0300

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            Double R = 0, h = 0;
+            Console.WriteLine("Введите Радиус");
+            R = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите высоту");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Объем Цилиндра = "+PI*R*R*h);
+            Console.WriteLine("     Площадь Цилиндра ="+2*PI*R*(R+h));
+            Console.ReadKey();
+        }
+    }
+}
