9462d247bcf6c48b683add67d09c220707fced8f
MakarovDenis <52467436+MakarovDenis@users.noreply.github.com>
2019-07-06 19:43:32 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,106 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2_
+namespace HomeWork4_1_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое число:");
-            int x = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите второе число:");
-            int y = Convert.ToInt32(Console.ReadLine());
-
-            int z = 1;
-
-            // Прямоугольник.
-            for (int i = 0; i < x; i++)
+            string sun = "Солнце";
+            string rain = "Дождь";
+            string wind = "Ветер";
+            string snow = "Снег";
+            string cold = "Холод";
+            string hot = "Жара";
+            m: Console.WriteLine("Привет. Я могу перевести 6 слов о погоде с русского на английский язык. Введите слово о погоде на русском языке");
+            string userword = Console.ReadLine();
+            if  (sun == userword)
             {
-                for (int j = 0; j < y; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
+                Console.WriteLine("Sun", userword);
+                goto m;
             }
-            Console.WriteLine();
-
-            // Прямоугольный треугольник.
-            //По вертикали
-            for (int i = 0; i < x; i++)
+            else if (rain ==userword)
             {
-                //По горизонтали
-                for (int j = 0; j < i + 1; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
+                Console.WriteLine("Rain", userword);
+                goto m;
             }
-            Console.WriteLine();
-
-            int buf = y;
-
-            // Равносторонний треугольник.
-            for (int i = 0; i < x; i++)
+            else if (wind == userword)
             {
-                for (int j = 0; j < y; j++)
-                {
-                    //Отступ от края консоли
-                    Console.Write(" ");
-                }
-                for (int k = 0; k < z; k++)
-                {
-                    Console.Write("*");
-                }
-                //Уменьшение значения отступа от левого края консоли
-                y -= 1;
-                //Увеличиваем количество звездочек на 2
-                z += 2;
-                Console.WriteLine();
+                Console.WriteLine("Wind", userword);
+                goto m;
             }
-            Console.WriteLine();
-            z = 1;
-
-            // Ромб.
-            for (int i = 0; i < (x + x + 1); i++)
+            else if (snow == userword)
             {
-                if (i < x)
-                {
-                    for (int j = 0; j < buf; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int k = 0; k < z; k++)
-                    {
-                        Console.Write("*");
-                    }
-                    //Уменьшение значения отступа от левого края консоли
-                    buf -= 1;
-                    //Увеличиваем количество звездочек на 2
-                    z += 2;
-                    Console.WriteLine();
-                }
-                else
-                {
-                    for (int j = 0; j < buf; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int k = 0; k < z; k++)
-                    {
-                        Console.Write("*");
-                    }
-                    //Увеличение значения отступа от левого края консоли
-                    buf += 1;
-                    //Уменьшение количество звездочек на 2
-                    z -= 2;
-                    Console.WriteLine();
-                }
+                Console.WriteLine("Snow", userword);
+                goto m;
             }
+            else if (cold == userword)
+            {
+                Console.WriteLine("Cold", userword);
+                goto m;
+            }
+            else if (hot == userword)
+            {
+                Console.WriteLine("Hot", userword);
+                goto m;
+            }
+            else
+            {
+                Console.WriteLine("Извините, в базе нет такого слова");
+                goto m;
 
+            }
+           
             
-            Console.ReadKey();
         }
     }
 }
